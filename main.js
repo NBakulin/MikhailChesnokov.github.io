eb1351a050e54b85c08f0181bffb0b6b4abbc5d3
Михаил <mishachesnokov111@gmail.com>
2017-11-29 05:42:58 +0500

    evaluation calculation added

--- main.js
+++ main.js
@@ -8,7 +8,14 @@ $('#continueStartPage').click(function(evt) {
 	
 	$('#startPage').remove();
 	$('#inputTablePage').css('display', 'block');
-	fillTable()
+	fillTable();
+});
+
+$('#countValues').click(function(evt) {
+    $('#inputTablePage').css('display', 'none');
+    $('#marks').css('display', 'block');
+    
+    fillMarks()
 });
 
 function fillTable() {
@@ -17,10 +24,10 @@ function fillTable() {
 			if (subclasses[criterium].indexOf(parseInt(programClass, 10)) !== -1) {
 				for (var metrics in factors[factor][criterium]) {
 					if (phases[metrics].indexOf(programStage) !== -1) {
-						for (var key in factors[factor][criterium][metrics]) {
-							var elementCode = factors[factor][criterium][metrics][key];
+						for (var codeIndex in factors[factor][criterium][metrics]) {
+							var elementCode = factors[factor][criterium][metrics][codeIndex];
 							var elementName = elements[elementCode];
-							$('#inputTable').append(makeTableRow(elementCode, elementName));
+							$('#inputTableBody').append(constructTableRow(elementCode, elementName));
 						}
 					}
 				}
@@ -29,11 +36,47 @@ function fillTable() {
 	}
 }
 
-function makeTableRow(elementCode, elementName) {
+function constructTableRow(elementCode, elementName) {
 	return "" +
 	"<tr>" +
 		"<td>" + elementCode + "</td>" +
 		"<td>" + elementName + "</td>" +
-		"<td> <input type=\"range\" min=\"0\" max=\"1\" step=\"0.1\" value=\"0\"> </td>" +
+		"<td><input type=\"range\" min=\"0\" max=\"1\" step=\"0.1\" value=\"0.5\" id=" + elementCode + "></td>" +
 	"</tr>"
+}
+
+// Sorry for this s**t:
+function fillMarks() {
+    for (var factor in factors) {
+        var criteriaSum = 0, criteriaAmount = 0;
+        $('#factorListItem').append("<li>" + factor + ": <strong id=" + "value_" + factor.replace(/ /g,'_') + "></strong>" + "<ol id=" + factor.replace(/ /g,'_') + ">");
+        for (var criterium in factors[factor]) {
+            if (subclasses[criterium].indexOf(parseInt(programClass, 10)) !== -1) {
+                var metricsSum = 0, metricsAmount = 0;
+                $("#" + factor.replace(/ /g,'_')).append("<li>" + criterium + ": <strong id=" + "value_" + criterium.replace(/ /g,'_') + "></strong>" + "<ol id=" + criterium.replace(/ /g,'_') + ">");
+                for (var metrics in factors[factor][criterium]) {
+                    if (phases[metrics].indexOf(programStage) !== -1) {
+                        var elementsSum = 0, elementsAmount = 0;
+                        for (var codeIndex in factors[factor][criterium][metrics]) {
+                            var elementCode = factors[factor][criterium][metrics][codeIndex];
+                            elementsSum += parseFloat($('#' + elementCode).val());
+                            elementsAmount ++;
+                        }
+                        var totalMetric = parseFloat(elementsSum) / parseFloat(elementsAmount);
+                        metricsSum += totalMetric;
+                        metricsAmount ++;
+                        $("#" + criterium.replace(/ /g,'_')).append("<li>" + metrics + ": <strong>" + totalMetric + "</strong></li>")
+                    }
+                }
+                $("#" + factor.replace(/ /g,'_')).append("</ol></li>");
+                var totalCriteria = metricsSum / metricsAmount;
+                criteriaSum += totalCriteria;
+                criteriaAmount ++;
+                $("#value_" + criterium.replace(/ /g,'_')).text(totalCriteria);
+            }
+        }
+        $('#factorListItem').append("</ol></li>");
+        var totalFactor = criteriaSum / criteriaAmount;
+        $("#value_" + factor.replace(/ /g,'_')).text(totalCriteria);
+    }
 }
